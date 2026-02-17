35c3357d16f62b19ebdff8f8717a8c6e32012019
Will <188920037+devster3000@users.noreply.github.com>
2026-02-17 12:28:30 +0000

    Added header and footer IDs to index.html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+fetch('/components/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+    document.getElementById(activePage).classList.add("active");
+  });
+
+fetch('/components/footer.html')
+  .then(response => response.text())
+  .then(data => document.getElementById('footer').innerHTML = data);
