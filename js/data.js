e753079e53ad3112413b00f23b29e77fd40e49c2
Will <188920037+devster3000@users.noreply.github.com>
2026-02-17 12:39:27 +0000

    Added header/footer styling. Added header.html content. Added lists for data ranges and skills clusters.

---
+++ js/data.js
@@ -0,0 +1,9 @@
+/* AGE RANGES */
+const ages = ["8-13", "14-19"]
+
+
+/* SKILLS CLUSTERS */
+const skills_cluster = ["Digital & Digital Skills", "Communication & Interpersonal Skills", "Problem Solving & Adaptability", "Creativity & Innovation", "Organisational & Commercial Awareness", "Leadership & Teamwork"]
+
+
+/* QUESTIONS */
